c69016681cd163ac0e4b1479f7876459c4a16309
Dominik Bystryk <dominikbys@gmail.com>
2024-04-24 16:16:04 +0200

    Missed on in done jump
     Missed on in done jump

--- ZadPrzerobienie na funkcję algorytmu/Startowazapas.asm
+++ ZadPrzerobienie na funkcję algorytmu/Startowazapas.asm
@@ -81,9 +81,29 @@ mov ecx, dword [ebp + 16]
 mov al, [ecx]
 cmp al, '+'
 je dodaj
-jmp zly_znak
+
+mov al, [ecx]
+cmp al, '-'
+je odejmij
+
+mov al, [ecx]
+cmp al, '*'
+je mnoz
+jne zly_znak
 
 dodaj:
+mov edx, 1
+call wypisz
+call nl
+jmp num2
+
+odejmij:
+mov edx, 1
+call wypisz
+call nl
+jmp num2
+
+mnoz:
 mov edx, 1
 call wypisz
 call nl
@@ -200,21 +220,18 @@ int 0x80
 jmp exit
 
 liczenie:
-mov eax, Num1_arr
-mov ebx, Num2_arr
-mov ecx, Wynik_arr
-mov edx, PrzepisanaLiczba_arr
-push eax
-push ebx
-push ecx
-push edx
-call liczsum
+mov ecx, dword [ebp + 16]
+mov al, [ecx]
+cmp al, '+'
+je liczsum
 
-exit:
-call czyscrejestr
-mov eax, 1
-mov ebx, 0
-int 0x80
+mov al, [ecx]
+cmp al, '-'
+je liczroz
+
+mov al, [ecx]
+cmp al, '*'
+je liczmnoz
 
 liczsum:
 mov eax, [strlen1]
@@ -232,53 +249,54 @@ jl sumuj_z_przeniesieniem_wzgledem_Num2
 
 sumuj:
 ;liczby równej dłuości, więc przepisujemy stringa do stringa wynikowego bez zmian w drugim
-mov esi, [ebp - 4]
-mov edi, [ebp - 12]
+mov esi, Num1_arr
+mov edi, Wynik_arr
 mov ecx, [strlen1]
 call przepisz
 call czyscrejestr
-mov esi, [ebp-12]
-mov edi, [ebp-8]
+mov esi, Wynik_arr
+mov edi, Num2_arr
 mov ecx, [strlen1]
-jmp sumator
+call sumator
 
 sumuj_z_przeniesieniem_wzgledem_Num1:
 ;liczba Num1 jest dłuższa od Num2, wiec po przepisaniu do Wynik_arr musimy dostosować jeszcze drugi array
-mov esi, [ebp - 4]
-mov edi, [ebp - 12]
+mov esi, Num1_arr
+mov edi, Wynik_arr
 mov ecx, [strlen1]
 call przepisz
 call czyscrejestr
-mov esi, [ebp - 8]
-mov edi, [ebp - 16]
+mov esi, Num2_arr
+mov edi, PrzepisanaLiczba_arr
 call roznicastrlen1astrlen2
 add edi, ecx
 mov ecx, [strlen2]
 call przepisz
 call czyscrejestr
-mov esi, [ebp - 12]
-mov edi, [ebp - 16]
+mov esi, Wynik_arr
+mov edi, PrzepisanaLiczba_arr
 mov ecx, [strlen1]
-jmp sumator
+call sumator
 
 sumuj_z_przeniesieniem_wzgledem_Num2:
 ;liczba Num2 jest dłuższa od Num1, wiec po przepisaniu do Wynik_arr musimy dostosować jeszcze drugi array
-mov esi, [ebp - 4]
-mov edi, [ebp - 12]
+mov esi, Num1_arr
+mov edi, Wynik_arr
 call roznicastrlen2astrlen1
 add edi, ecx
 mov ecx, [strlen1]
 call przepisz
 call czyscrejestr
-mov esi, [ebp - 8]
-mov edi, [ebp - 16]
+mov esi, Num2_arr
+mov edi, PrzepisanaLiczba_arr
 mov ecx, [strlen2]
 call przepisz
 call czyscrejestr
-mov esi, [ebp - 12]
-mov edi, [ebp - 16]
+mov esi, Wynik_arr
+mov edi, PrzepisanaLiczba_arr
 mov ecx, [strlen2]
-jmp sumator
+call sumator
+
 
 sumator:
 add esi, ecx
@@ -322,7 +340,7 @@ dec ecx
 cmp ecx, 0
 je done
 dec edi
-mov esi, [ebp - 12]
+mov esi, Wynik_arr
 add esi, ecx
 mov al, byte [esi]
 mov bl, byte [edi]
@@ -332,35 +350,206 @@ jge do_reszty
 mov [esi], al
 jmp add_loop
 
-done:
-call czyscrejestr
-mov edi, [ebp - 12]
+liczroz:
 mov eax, [strlen1]
 mov ebx, [strlen2]
 cmp eax, ebx
-jge strlen1dl
+je roznica
+mov eax, [strlen1]
+mov ebx, [strlen2]
+cmp eax, ebx
+jg roznica_z_przeniesieniem_wzgledem_Num1
+mov eax, [strlen1]
+mov ebx, [strlen2]
+cmp eax, ebx
+jl roznica_z_przeniesieniem_wzgledem_Num2
+
+roznica:
+mov esi, Num1_arr
+mov edi, Wynik_arr
+mov ecx, [strlen1]
+call przepisz
+call czyscrejestr
+mov esi, Wynik_arr
+mov edi, Num2_arr
+mov ecx, [strlen1]
+call roznicuj
+
+roznica_z_przeniesieniem_wzgledem_Num1:
+mov esi, Num1_arr
+mov edi, Wynik_arr
+mov ecx, [strlen1]
+call przepisz
+call czyscrejestr
+mov esi, Num2_arr
+mov edi, PrzepisanaLiczba_arr
+call roznicastrlen1astrlen2
+add edi, ecx
 mov ecx, [strlen2]
-add ecx, 1
-call converback
-call nl
-mov ecx, [ebp - 12]
-mov edx, [strlen2]
-add edx, 1
-call wypisz
-call nl
+call przepisz
+call czyscrejestr
+mov esi, Wynik_arr
+mov edi, PrzepisanaLiczba_arr
+mov ecx, [strlen1]
+call roznicuj
+
+roznica_z_przeniesieniem_wzgledem_Num2:
+mov esi, Num1_arr
+mov edi, Wynik_arr
+call roznicastrlen2astrlen1
+add edi, ecx
+mov ecx, [strlen1]
+call przepisz
+call czyscrejestr
+mov esi, Num2_arr
+mov edi, PrzepisanaLiczba_arr
+mov ecx, [strlen2]
+call przepisz
+call czyscrejestr
+mov esi, Wynik_arr
+mov edi, PrzepisanaLiczba_arr
+mov ecx, [strlen2]
+call roznicuj
+
+roznicuj:
+add esi, ecx
+add edi, ecx
+mov al, [esi]
+mov bl, [edi]
+cmp al, bl
+jl sub_with_borrow
+sub al, bl
+mov [esi], al
+jmp sub_loop
+
+sub_with_borrow:
+add al, 0x10
+sub al, bl
+mov [esi], al
+jmp sub_carry
+
+sub_carry:
+cmp esi, 0
+je done
+dec esi
+mov al, [esi]
+cmp al, 0x00
+je change_F
+sub al, 0x01
+mov [esi], al
+jmp sub_loop
+
+change_F:
+mov al, 0x0F
+mov [esi], al
+jmp sub_carry
+
+sub_loop:
+dec ecx
+cmp ecx, 0
+je done
+dec edi
+mov esi, Wynik_arr
+add esi, ecx
+mov al, byte [esi]
+mov bl, byte [edi]
+cmp al, bl
+jl sub_with_borrow
+sub al, bl
+mov [esi], al
+jmp sub_loop
+
+liczmnoz:
+;zaczynamy od sprawdzenia co dłuższe - od tego determinujemy ecx przy mnożeniu
+mov eax, [strlen1]
+mov ebx, [strlen2]
+cmp eax, ebx
+jge mnoz_strlen1
+jmp mnoz_strlen2
+
+
+mnoz_strlen1:
+mov esi, Num1_arr
+mov edi, Num2_arr
+mov ecx, [strlen1]
+add esi, ecx
+mov ecx, [strlen2]
+add edi, ecx
+mov ecx, [strlen1]
+mov al, byte [esi]
+mov bl, byte [edi]
+mul bl
+mov edi, 0
+call podlicz_petl
+mov edi, Wynik_arr
+add edi, ecx
+add edi, 1
+cmp al, 0x10
+jge sprawdz_carry
+mov [edi], al
+jmp mul_loop_One
+
+podlicz_petl:
+inc edi
+mov [petl], edi
 ret
 
-strlen1dl:
+mul_loop_One:
+dec ecx
+cmp ecx, 0
+je done_mul
+dec esi
+dec edi
+mov al, byte [esi]
+mul bl
+mov dl, [carr]
+add al, dl
+xor edx, edx
+cmp al, 0x10
+jge sprawdz_carry
+mov [edi], al
+cmp al, 0x10
+jl clear_carr
+jmp mul_loop_One
+
+clear_carr:
+xor edx, edx
+mov edx, 0
+mov [carr], edx
+jmp mul_loop_One
+
+done_mul:
+dec edi
+mov dl, [carr]
+mov [edi], dl
+jmp wypisz_mul
+
+sprawdz_carry:
+cmp al, 0x10
+jl carr_wpis
+inc edx
+sub al, 0x10
+cmp al, 0x10
+jge sprawdz_carry
+carr_wpis:
+mov [carr], edx
+mov [edi], al
+jmp mul_loop_One
+
+mnoz_strlen2:
+mov esi, Num1_arr
+mov edi, Num2_arr
+mov edx, Wynik_arr
+mov ecx, [strlen2]
+add edi, ecx
+add edx, ecx
 mov ecx, [strlen1]
-add ecx, 1
-call converback
-call nl
-mov ecx, Wynik_arr
-mov edx, [strlen1]
-add edx, 1
-call wypisz
-call nl
-ret
+add edx, ecx
+; poniewaz przy mnozeniu moga byc 2 przeniesienia -> jedno na ostatnim bajcie przy dodawaniu, drugie jako wynik ostatniego dodawania
+mov ecx, [strlen1]
+add esi, ecx
+jmp exit
+; nie przygotowano wariantu, w którym A jest krótsze od B
 
 przepisz:
 mov al, [esi+1]
@@ -406,3 +595,64 @@ inc edi
 dec ecx
 jnz converback
 ret
+
+
+done:
+call czyscrejestr
+mov edi, Wynik_arr
+mov eax, [strlen1]
+mov ebx, [strlen2]
+cmp eax, ebx
+jge strlen1dl
+mov ecx, [strlen2]
+add ecx, 1
+call converback
+call nl
+mov ecx, Wynik_arr
+mov edx, [strlen2]
+add edx, 1
+call wypisz
+call nl
+jmp exit
+
+strlen1dl:
+mov ecx, [strlen1]
+add ecx, 1
+call converback
+call nl
+mov ecx, Wynik_arr
+mov edx, [strlen1]
+add edx, 1
+call wypisz
+call nl
+jmp exit
+
+
+wypisz_mul:
+call czyscrejestr
+mov edi, Wynik_arr
+mov eax, [strlen1]
+mov ebx, [strlen2]
+mov ecx, 0
+add ecx, eax
+add ecx, ebx
+add ecx, 1
+call converback
+call nl
+mov ecx, Wynik_arr
+mov eax, [strlen1]
+mov ebx, [strlen2]
+mov edx, 0
+add edx, eax
+add edx, ebx
+add edx, 1
+call wypisz
+call nl
+jmp exit
+
+exit:
+call czyscrejestr
+mov eax, 1
+mov ebx, 0
+int 0x80
+
